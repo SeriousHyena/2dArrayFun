4f923eadfa8bb22e451323bf8c142382a3d462a3
SeriousHyena <emackiii@gmail.com>
2021-10-11 06:47:02 -0700

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+	int myNums[2][3]
+	{
+		{1,2,3},
+		{4,5,6}
+	};
+
+	cout << myNums[0][2] << endl;
+
+	myNums[1][0] = 14;
+	cout << myNums[1][0]<<endl;
+
+	//nested loop
+	for (int row = 0; row < 2; row++)
+	{
+		for (int col = 0; col < 3; col++)
+		{
+			cout << myNums[row][col] <<" ";
+		}
+		cout << endl;
+	}
+
+	cout << endl << endl;
+
+
+	//print the array backwards
+	for (int row = 2-1; row >=0; row--)
+	{
+		for (int col = 3 -1 ; col >= 0; col--)
+		{
+			cout << myNums[row][col] << " ";
+		}
+		cout << endl;
+	}
+
+	
+
+	return 0;
+}
